d96e2f54c380007659d35cf458600f305bf816b8
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 16:06:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,22 +69,21 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+holiday_hash = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 
 
 
